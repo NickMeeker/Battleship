3155d76694f2d78160678eeff3fcd767b4918610
nickmeeker <nowinterweather@gmail.com>
2018-03-08 18:16:24 -0500

    - added button displays to main menu state

--- core/src/com/teamgroupfourteen/game/States/MainMenuState.java
+++ core/src/com/teamgroupfourteen/game/States/MainMenuState.java
@@ -3,7 +3,10 @@ package com.teamgroupfourteen.game.States;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.teamgroupfourteen.game.Battleship;
+
+import java.awt.Button;
 
 /**
  * Created by nick on 2/28/18.
@@ -11,12 +14,31 @@ import com.teamgroupfourteen.game.Battleship;
 
 public class MainMenuState extends State {
     private Texture background;
+    private TextureRegion mainBackground;
     private Texture playBtn;
+    private Texture singlePlayerBtn;
+    private Texture multiPlayerBtn;
+    private Texture spectatorBtn;
+    private Texture storeBtn;
+    private Texture friendsBtn;
+    private Texture creditsBtn;
+
+
+
+
     public MainMenuState(GameStateManager gsm){
         super(gsm);
         cam.setToOrtho(false, Battleship.WIDTH/2, Battleship.HEIGHT/2);
-        background = new Texture("bg.png");
-        playBtn = new Texture("playbtn.png");
+        // set the background as a texture region. 178x232 is the resolution of the image
+        background = new Texture("backgroundOcean.png");
+        mainBackground = new TextureRegion(background, 0, 0, 178, 232 );
+        singlePlayerBtn = new Texture("button_single-player.png");
+        multiPlayerBtn = new Texture("button_multiplayer.png");
+        friendsBtn = new Texture("button_friends.png");
+        spectatorBtn = new Texture("button_spectator.png");
+        creditsBtn = new Texture("button_credits.png");
+
+
     }
 
     @Override
@@ -37,8 +59,12 @@ public class MainMenuState extends State {
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
-        sb.draw(background, 0, 0);
-        sb.draw(playBtn, cam.position.x - playBtn.getWidth() / 2, cam.position.y);
+        // TODO: Figure out why this works
+        sb.draw(mainBackground, 0, 0, Battleship.WIDTH, Battleship.WIDTH);
+        sb.draw(singlePlayerBtn, cam.position.x - singlePlayerBtn.getWidth() / 2, cam.position.y + 50, singlePlayerBtn.getWidth(), singlePlayerBtn.getHeight());
+        sb.draw(multiPlayerBtn, cam.position.x - multiPlayerBtn.getWidth() / 2, cam.position.y, multiPlayerBtn.getWidth(), multiPlayerBtn.getHeight());
+        sb.draw(spectatorBtn,cam.position.x - spectatorBtn.getWidth() / 2, cam.position.y - 50, spectatorBtn.getWidth(), spectatorBtn.getHeight());
+        sb.draw(creditsBtn, cam.position.x - creditsBtn.getWidth() / 2, cam.position.y - 100, creditsBtn.getWidth(), creditsBtn.getHeight());
         sb.end();
     }
 
